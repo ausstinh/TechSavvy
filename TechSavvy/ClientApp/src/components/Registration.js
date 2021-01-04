07bed2f4097e6e032963dd9fa817a3cca730ef2e
Austin Harvey <austin.harvey@deftflux.com>
2021-01-04 12:42:00 -0700

    Complete validation for login and register

--- TechSavvy/ClientApp/src/components/Registration.js
+++ TechSavvy/ClientApp/src/components/Registration.js
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from 'react';
 import { Button, Card, CardBody, Col, Container, Form, Input, InputGroup, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { event } from 'jquery';
 class Registration extends Component {
     /**
      * Constuctor for user states (username, password, errors) 
@@ -11,161 +12,158 @@ class Registration extends Component {
             Username: '',
             Email: '',
             Password: '',
+            Confirm_password: '',
             errors: {
+                areErrors: false,
                 username: '',
                 email: '',
                 password: '',
+                confirm_password: '',
+                alreadyTakenEmail: '',
             }
         }
 
         this.Email = this?.Email?.bind(this);
         this.Password = this?.Password?.bind(this);
         this.Username = this?.Username?.bind(this);
-        this.register = this?.register?.bind(this);
+        this.handleSubmit = this?.handleSubmit?.bind(this);
+        this.validate = this.validate.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
     }
 
     /**
      * Handle Submit function that will send a post with the user fields to create the user
-     * @param {any} event
+     * @param {any} e
      */
-    register(event) {
+    handleSubmit(e) {
 
-        if (this.validate(event)) {
-            console.log(this.state);
-
-            fetch('http://localhost:51282/Api/user/CreateUser', {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+        //check all field validations
+        if (this.state.Email === '') {
+            this.setState(prevState => ({             
+                errors: {
+                    ...prevState.errors,
+                    email: 'Please enter Email',
                 },
-                body: JSON.stringify({
-                    Username: this.state.Username,
-                    Password: this.state.Password,
-                    Email: this.state.Email,
-                })
-            }).then((Response) => Response.json())
-                .then((Result) => {
-                    if (Result.Status == 'Success')
-                        this.props.history.push("/Home");
-                    else
-                        alert('error creating user')
-                })
+            })); 
         }
-    }
-      /*
-       * NOTE: NOT FINISHED YET BUT CLOSE
-       *Handle validation and update state of user state by change
-       *@param  {any} event
-       * */
-    handleChange = (event) => {
-        const { name, value } = event.target;
-        let errors = this.state.errors; 
-        //regex test for actual email
-        var emailTest = new RegExp(/^(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
-        //regex test for 1 uppercase and at least 8 characters
-        var passwordTest = new RegExp(/^(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
-
-        //check event validation by name and set user state
-        switch (name) {
-            case 'email':              
-                errors.email =
-                   emailTest.test(value)
-                        ? ''
-                        : 'Email is not valid!';
-                this.setState({ Email: event.target.value });
-                break;
-            case 'password':
-                errors.password =
-                    value.length < 8
-                        ? 'Password must be 8 characters long!'
-                            ? passwordTest.test(value)
-                                : ''
-                                : 'Password needs at least 1 uppercase and be 8 characters long!';
-                this.setState({ Password: event.target.value });
-                break;
-            case 'username':
-                this.setState({ Username: event.target.value });
-                break;
-            default:
-                break;
+        if (this.state.Password === '') {
+            this.setState(prevState => ({           
+                errors: {
+                    ...prevState.errors,
+                    password: 'Please enter Password',
+                },
+            }));
+        }
+        if (this.state.Username === '') {
+            this.setState(prevState => ({              
+                errors: {
+                    ...prevState.errors,
+                    username: 'Please enter Username',
+                },
+            }));
+        }
+        if (this.state.Confirm_password !== this.state.Password) {
+            this.setState(prevState => ({            
+                errors: {
+                    ...prevState.errors,
+                    confirm_password: 'Please match passwords',
+                },
+            }));
         }
 
-        this.setState({ errors, [name]: value }, () => {
-            console.log(errors)
-        })
+        fetch('http://localhost:44383/Api/user/CreateUser', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                Username: this.state.Username,
+                Password: this.state.Password,
+                Email: this.state.Email,
+            })
+        }).then((Response) => Response.json())
+            .then((Result) => {
+                if (Result.Status == 'Success')
+                    this.props.history.push("/Home");
+                else {
+                    this.setState(prevState => ({
+                        error: {
+                            ...prevState.errors,
+                            alreadyTakenEmail: 'Email is not Valid',
+                        },
+                    }));
+                }
+            })
     }
 
-/*
-   * NOTE: NOT FINISHED YET BUT CLOSE
-   *Handle validation and update state of user state by change
-   *@param  {any} event
-   * */
-    validate() {
-        const { name, value } = event.target;
+
+    /*
+    * NOTE: NOT FINISHED YET BUT CLOSE
+    *Handle validation and update state of user state by change
+    *@param  {any} event
+    * */
+    handleChange(event) {
+        this.validate(event);
+    }
+    /*
+     * NOTE: NOT FINISHED YET BUT CLOSE
+     *Handle validation and update state of user state by change
+     *@param  {any} event
+     * */
+    validate(event) {
+        if (this.state.errors != null) {
+            event.preventDefault();
+        }
+        const name = event.target.name;
+        const value = event.target.value;
         let errors = this.state.errors;
-        //regex test for actual email
-        var emailTest = new RegExp(/^(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
-        //regex test for 1 uppercase and at least 8 characters
-        var passwordTest = new RegExp(/^(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
+        console.log(name);
+        console.log(value);
+        console.log(value.match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"));
 
         //check event validation by name and set user state
-        
         switch (name) {
             case 'email':
-                errors.email =
-                    //is empty?
-                    event.target.value == 0
-                    ? 'Please enter an email'
-                    //is actual email?
-                    : emailTest.test(value)
-                        ? ''
-                        : 'Email is not valid!';
-                //set email state
-                this.setState({ Email: event.target.value });
+                this.setState(prevState => ({
+                    Email: value,
+                    errors: {
+                        ...prevState.errors,
+                        email: value.match("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)") || value === '' ? "" : 'Not is not a valid email',
+                    },
+                }));
                 break;
             case 'password':
-                errors.password =
-                    //is empty?
-                    event.target.value == 0
-                    ? 'Please enter a password'
-                        //password have at least 8 characters and has 1 uppercase?
-                        : value.length < 8 && passwordTest.test(value)
-                            ? ''
-                            : 'Password needs at least 1 uppercase and be 8 characters long!';
-                 //set password state
-                this.setState({ Password: event.target.value });
+                this.setState(prevState => ({
+                    Password: value,
+                    errors: {
+                        ...prevState.errors,
+                        password: value.match("(^(?=.*?[A-Z]).{8,}$)") || value === '' ? '' : 'Password needs at least 1 uppercase and be 8 characters long',
+                    },
+                }));
                 break;
             case 'username':
-                  //is empty?
-                errors.username = event.target.value == 0
-                    ? ''
-                    : 'Please enter a username';
-                 //set username state
-                this.setState({ Username: event.target.value });
+                this.setState(prevState => ({
+                    Username: value,
+                    errors: {
+                        ...prevState.errors,
+                        username: value === ''  ? '': '',
+                    },
+                }));
                 break;
+            case 'confirm_password':
+                this.setState(prevState => ({
+                    errors: {
+                        ...prevState.errors,
+                        confirm_password: this.state.Password === value ? '' : 'Please match password',
+                    }
+                }))       
             default:
                 break;
         }
-        confirm password field will come later
-
-        if (!input["confirm_password"]) {
-            isValid = false;
-            errors["confirm_password"] = "Please enter your confirm password.";
-        }
-
-        if (typeof input["password"] !== "undefined" && typeof input["confirm_password"] !== "undefined") {
-
-            if (input["password"] != input["confirm_password"]) {
-                isValid = false;
-                errors["password"] = "Passwords don't match.";
-            }
-        }
-
-        this.setState({ errors, [name]: value }, () => {
-            console.log(errors)
-        })
     }
+
 
     //Registartion form
     render() {
@@ -173,24 +171,28 @@ class Registration extends Component {
             <div className="app flex-row align-items-center">
                 <Container>
                     <Row className="justify-content-center">
-                        < Col md="9" lg="7" xl="6">
-                            < Card className="mx-4">
-                                < CardBody className="p-4">
-                                    < Form >
-
+                        <Col md="9" lg="7" xl="6">
+                            <Card className="mx-4">
+                                <CardBody className="p-4">
+                                    <div className="text-danger">{this.state.errors.alreadyTakenEmail}</div>
+                                    <Form>
                                         <InputGroup className="mb-3" >
-                                            < Input type="text" onChange={this.Email} placeholder="Enter New Email" />
+                                            <Input type="text" name="email" onChange={this.handleChange} placeholder="Enter New Email" />
                                         </InputGroup >
-                                       <div className="text-danger">{this.state.errors.email}</div>
+                                        <div className="text-danger">{this.state.errors.email}</div>
                                         <InputGroup className="mb-3" >
-                                            <Input type="text" onChange={this.Username} placeholder="Enter New Username" />
+                                            <Input type="text" name="username" onChange={this.handleChange} placeholder="Enter New Username" />
                                         </InputGroup >
                                         <div className="text-danger">{this.state.errors.username}</div>
                                         <InputGroup className="mb-3">
-                                            <Input type="password" onChange={this.Password} placeholder="Enter New Password" />
+                                            <Input type="password" name="password" onChange={this.handleChange} placeholder="Enter New Password" />
                                         </InputGroup >
                                         <div className="text-danger">{this.state.errors.password}</div>
-                                        <Button onClick={this.register} color="success" block > Create Account</Button>
+                                        <InputGroup className="mb-3">
+                                            <Input type="password" name="confirm_password" onChange={this.handleChange} placeholder="Enter Password Again" />
+                                        </InputGroup >
+                                        <div className="text-danger">{this.state.errors.confirm_password}</div>
+                                        <Button onClick={this.handleSubmit} color="success" block > Create Account</Button>
                                     </Form><br />
                                     <div class="row" className="mb-2 pageheading">
                                         <Link to='/Login' class="col-sm-12 btn btn-primary"> Login Instead </Link>
@@ -204,5 +206,4 @@ class Registration extends Component {
         );
     }
 }
-
 export default Registration;
